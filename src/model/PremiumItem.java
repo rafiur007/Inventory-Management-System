a08820a0312054e4c8f881937fd99483fa6d4916
ramisazarin61 <ramisazarin61@gmail.com>
2026-06-04 01:41:45 +0600

    Add files via upload

---
+++ src/model/PremiumItem.java
@@ -0,0 +1,9 @@
+public class PremiumItem extends Item {
+    public PremiumItem(String id, String name, int quantity) {
+        super(id, name, quantity);
+    }
+
+    @Override
+    public String getType() {
+        return "Premium";
+    }
