703a0d4e102cfbebc0adbfccbf9e49cf94a58d88
amychicham <amychicham@gmail.com>
2026-06-04 01:18:33 +0600

    Add files via upload

---
+++ src/model/item.java
@@ -0,0 +1,28 @@
+package model;
+
+public abstract class Item {
+
+    private String id;
+    private String name;
+    private int quantity;
+
+    public Item(String id, String name, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public abstract String getType();
+}
